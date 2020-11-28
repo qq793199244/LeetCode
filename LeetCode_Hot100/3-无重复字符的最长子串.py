5ab4cee9cae3e0ef2eb3ebc85b703b9ffe138ae6
qq793199244 <793199244@qq.com>
2020-11-29 00:24:35 +0800

    LeetCode热题HOT100

--- LeetCode_Hot100/3-无重复字符的最长子串.py
+++ LeetCode_Hot100/3-无重复字符的最长子串.py
@@ -1,28 +1,22 @@
 '''
 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
 '''
-
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        if not s:
-            return 0
         # 滑动窗口
         n = len(s)
-        right = 0
+        if n == 0:
+            return 0
         max_len = 0
-        lookup = set()
-        for left in range(n):
-            while right < n and s[right] not in lookup:
-                lookup.add(s[right])
-                right += 1
-            if len(lookup) > max_len:
-                max_len = len(lookup)
-            lookup.remove(s[left])
+        end = -1
+        d = {}
+        for i, c in enumerate(s):
+            if c in d:
+                end = max(end, d[c])
+            max_len = max(max_len, i-end)
+            d[c] = i
         return max_len
+
 
 if __name__ == '__main__':
     u = Solution()
