d183b31d1d96a06367e6143b2788f93c96b67df1
qq793199244 <793199244@qq.com>
2020-12-03 00:43:30 +0800

    新增快慢指针遍历1次方法

--- LeetCode_Hot100/19-删除链表的倒数第n个节点.py
+++ LeetCode_Hot100/19-删除链表的倒数第n个节点.py
@@ -11,11 +11,6 @@ class ListNode(object):
         self.next = next
 class Solution(object):
     def removeNthFromEnd(self, head, n):
-        """
-        :type head: ListNode
-        :type n: int
-        :rtype: ListNode
-        """
         if not head:
             return head
         length = 0
@@ -23,8 +18,40 @@ class Solution(object):
         while i:
             length += 1
             i = i.next
-        cur = dummy = ListNode(0, head)
-        for i in range(length-n):
+        cur = dummy = ListNode(0)
+        cur.next = head
+        for i in range(length - n):
             cur = cur.next
         cur.next = cur.next.next
         return dummy.next
+
+    def removeNthFromEnd2(self, head, n):
+        dummy = ListNode(0)
+        dummy.next = head
+        slow, fast = dummy, dummy
+        while n > 0:
+            fast = fast.next
+            n -= 1
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next
+        slow.next = slow.next.next
+        return dummy.next
+
+
+if __name__ == '__main__':
+    u = Solution()
+    head = ListNode(1)
+    n2 = head.next = ListNode(2)
+    n3 = n2.next = ListNode(3)
+    n4 = n3.next = ListNode(4)
+    n5 = n4.next = ListNode(5)
+    while head:
+        print(head.val, end='→')
+        head = head.next
+    print()
+    print('----------------------------')
+    res = u.removeNthFromEnd2(head, 2)
+    while res:
+        print(res.val, end='→')
+        res = res.next
