56fa0e8831357f8792819b746b8efc9330273c5b
unknown <793199244@qq.com>
2020-12-26 15:32:23 +0800

    两种方法

--- Hot100之外的高频面试题/922-按奇偶排序数组II.py
+++ Hot100之外的高频面试题/922-按奇偶排序数组II.py
@@ -1,35 +1,52 @@
 '''
-给定一个非负整数数组 A， A 中一半整数是奇数，一半整数是偶数。
-对数组进行排序，以便当 A[i] 为奇数时，i 也是奇数；当 A[i] 为偶数时， i 也是偶数。
+给定一个非负整数数组A， A 中一半整数是奇数，一半整数是偶数。
+对数组进行排序，以便当A[i] 为奇数时，i也是奇数；当A[i]为偶数时， i 也是偶数。
 你可以返回任何满足上述条件的数组作为答案。
 示例：
 输入：[4,2,5,7]
 输出：[4,5,2,7]
 解释：[4,7,2,5]，[2,5,4,7]，[2,7,4,5] 也会被接受。
+提示：
+2 <= A.length <= 20000
+A.length % 2 == 0
+0 <= A[i] <= 1000
 '''
-class Solution(object):
+
+
+class Solution:
     def sortArrayByParityII(self, A):
-        """
-        :type A: List[int]
-        :rtype: List[int]
-        """
+        # 开辟了新空间
+        # 时间复杂度O(n)
+        # 空间复杂度O(n)
         n = len(A)
-        if n == 0:
-            return []
+        i, j = 0, 1
+        res = [0] * n
+        for a in A:
+            if a % 2 == 0:
+                res[i] = a
+                i += 2
+            else:
+                res[j] = a
+                j += 2
+        return res
+
+    def sortArrayByParityII2(self, A):
+        # 不开辟新空间，双指针原地排序。遍历偶数位，如果偶数位不是偶数，就从奇数位找偶数，交换
+        # 时间复杂度O(n)
+        # 空间复杂度O(1)
+        n = len(A)
         i, j = 0, 1
         while i < n:
             if A[i] % 2 == 1:
-                while A[j] % 2 != 0:
+                while A[j] % 2 == 1:
                     j += 2
                 A[i], A[j] = A[j], A[i]
             i += 2
         return A
-# 时间复杂度O(n)
-# 空间复杂度(1)
+
 
 if __name__ == '__main__':
     u = Solution()
-    A1 = [4,2,5,7]
-    A2 = [1, 0, 3, 2, 5, 4]
-    print(u.sortArrayByParityII(A1))
-    print(u.sortArrayByParityII(A2))
+    nums = [4, 2, 5, 7]
+    print(u.sortArrayByParityII(nums))
+    print(u.sortArrayByParityII2(nums))
