4e2e3ef0314ddf5aa878da2047628405ece6b3b3
unknown <793199244@qq.com>
2021-01-11 20:01:18 +0800

    用两个栈实现队列

--- Hot100之外的高频面试题/225-用队列实现栈.py
+++ Hot100之外的高频面试题/225-用队列实现栈.py
@@ -5,51 +5,50 @@ pop() -- 移除栈顶元素
 top() -- 获取栈顶元素
 empty() -- 返回栈是否为空
 注意:
-你只能使用队列的基本操作-- 也就是 push to back, peek/pop from front, size, 和 is empty 这些操作是合法的。
-你所使用的语言也许不支持队列。 你可以使用 list 或者 deque（双端队列）来模拟一个队列 , 只要是标准的队列操作即可。
+你只能使用队列的基本操作-- 也就是push to back, peek/pop from front, size, 和is empty这些操作是合法的。
+你所使用的语言也许不支持队列。你可以使用 list 或者 deque（双端队列）来模拟一个队列, 只要是标准的队列操作即可。
 你可以假设所有操作都是有效的（例如, 对一个空的栈不会调用 pop 或者 top 操作）。
 '''
 
+
+# 时间复杂度，入栈操作O(n)，其他O(1)；空间复杂度O(n)
 class MyStack(object):
 
     def __init__(self):
         self.q1 = []
         self.q2 = []
-        self.front = None
 
-    def push(self, x):  # O(1)
-        self.q1.append(x)
-        self.front = x
+    def push(self, x):
+        # 先把元素push进q2
+        self.q2.append(x)
+        # 如果q1不为空，把q1中最左边的元素添加到q2，直到q1为空
+        while self.q1:
+            self.q2.append(self.q1.pop(0))
+        # q1和q2交换，元素都在q1中，现在q2为空
+        self.q1, self.q2 = self.q2, self.q1
 
-    def pop(self):  #O(n)
-         # 把q1里的元素取出，留下一个，其余放入q2中
-        for i in range (len(self.q1)-1):
-            self.front = self.q1.pop(0)
-            self.q2.append(self.front)
-        popElement = self.q1.pop(0)
-        self.q1 = self.q2
-        self.q2 = []
-        return popElement
+    def pop(self):
+        return self.q1.pop(0)
 
-    def top(self):  # O(1)
-        return self.front
+    def top(self):
+        return self.q1[0]
 
-    def empty(self):    # O(1)
+    def empty(self):
         if not self.q1:
             return True
         return False
 
 
 if __name__ == '__main__':
-    u = MyStack()
-    '''
-    ["MyStack","push","push","top","pop","empty"]
-    [[],[1],[2],[],[],[]]
-    [null,null,null,2,2,false]
-    '''
-    print(u.__init__())
-    print(u.push(1))
-    print(u.push(2))
-    print(u.top())
-    print(u.pop())
-    print(u.empty())
+    my_stack = MyStack()
+    # 输入
+    # ["MyStack","push","push","top","pop","empty"]
+    # [[],[1],[2],[],[],[]]
+    # 输出
+    # [null,null,null,2,2,false]
+    print(my_stack.__init__(), end=' ')
+    print(my_stack.push(1), end=' ')
+    print(my_stack.push(2), end=' ')
+    print(my_stack.top(), end=' ')
+    print(my_stack.pop(), end=' ')
+    print(my_stack.empty(), end=' ')
