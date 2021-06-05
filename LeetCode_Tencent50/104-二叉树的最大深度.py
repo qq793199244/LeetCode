a9e305f392dfcf3bb17e11f294512a3f33450d75
unknown <793199244@qq.com>
2021-06-05 22:34:52 +0800

    二叉树、递归

---
+++ LeetCode_Tencent50/104-二叉树的最大深度.py
@@ -0,0 +1,40 @@
+'''
+给定一个二叉树，找出其最大深度。
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+说明: 叶子节点是指没有子节点的节点。
+示例：
+给定二叉树 [3,9,20,null,null,15,7]，
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最大深度 3 。
+'''
+
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    # 时间复杂度O(n)，空间复杂度O(Height)
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        return max(left, right) + 1
+
+
+if __name__ == '__main__':
+    u = Solution()
+    n3 = TreeNode(3)
+    n9 = n3.left = TreeNode(9)
+    n20 = n3.right = TreeNode(20)
+    n15 = n20.left = TreeNode(15)
+    n7 = n20.right = TreeNode(7)
+    print(u.maxDepth(n3))
