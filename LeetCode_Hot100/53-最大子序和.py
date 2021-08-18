76ddedd8c2307d5e3d2656e1080f1bde37848d09
unknown <793199244@qq.com>
2021-08-18 17:18:27 +0800

    最大子序和

--- LeetCode_Hot100/53-最大子序和.py
+++ LeetCode_Hot100/53-最大子序和.py
@@ -1,26 +1,39 @@
 '''
+53. 最大子序和
 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+示例:
+输入: [-2,1,-3,4,-1,2,1,-5,4]
+输出: 6
+解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+进阶:
+如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
 '''
 
+
 class Solution(object):
+    # 属于动态规划。时间复杂度O(n)，空间复杂度O(1)
     def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        if not nums:
+        n = len(nums)
+        if n == 0:
             return 0
-        cur_sum = max_sum = nums[0]
-        for i in range(1, len(nums)):
-            if cur_sum > 0:
-                cur_sum += nums[i]
-            else:
-                cur_sum = nums[i]
-            if cur_sum > max_sum:
-                max_sum = cur_sum
-        return max_sum
+        curSum = 0
+        maxSum = nums[0]
+        for i in range(n):
+            # 一个负数加上后面的数，肯定比后面的数小，所以当之前和小于0时，丢弃，重新算
+            if curSum < 0:
+                curSum = 0
+            curSum += nums[i]
+            maxSum = max(curSum, maxSum)
+        return maxSum
+
 
 if __name__ == '__main__':
     u = Solution()
-    n = [-2,1,-3,4,-1,2,1,-5,4]
-    print(u.maxSubArray(n))
+    nums1 = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    nums2 = []
+    nums3 = [-2]
+    nums4 = [-2, -1]
+    print(u.maxSubArray(nums1))  # 6
+    print(u.maxSubArray(nums2))  # 0
+    print(u.maxSubArray(nums3))  # -2
+    print(u.maxSubArray(nums4))  # -1
